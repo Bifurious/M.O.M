03b3e6ae1fd6273d383d5c1d31988a4e0f22ecaa
Bifurious <71828982+Bifurious@users.noreply.github.com>
2021-03-12 20:48:18 +0000

    Add files via upload

---
+++ DecisionTree.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecisionTree : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string beginningDecision;
+        string creep;
+        string shoot;
+        string move;
+        string attack;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
